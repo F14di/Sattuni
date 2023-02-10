de4fc775bd1918ef46e4273b887cd450cdcdb8b2
Fadi Muhammad <fadi.muhammad@personetics.com>
2023-02-10 16:09:58 +0200

    warning fix

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -47,7 +47,7 @@ const Footer = ({
             {/* <FooterNav /> */}
             <div className="footer-copyright">Sattuni. All right reserved</div>
           </div>
-            <div style={{position:'absolute', justifyContent:'center', gap:'7px', display:'flex'}}><a>Impressum</a><a>Datenschutzerklärung</a></div>
+            <div style={{position:'absolute', justifyContent:'center', gap:'7px', display:'flex'}}>{/*<a>Impressum</a><a>Datenschutzerklärung</a>*/}</div>
         </div>
       </div>
     </footer>
